72dee7edd87beaddb075bd8d972e801012e445ae
boudesit <boudesit@gmail.com>
2016-04-16 19:29:55 +0200

    DOSOMETHING

--- js/Ennemy.js
+++ js/Ennemy.js
@@ -1,18 +1,67 @@
-function Ennemy(game) {
-	this.game = game ;
-	this.sprite = null ;
-	this.posX = 0 ;
-	this.posY = 0 ;
-	
+function Ennemy(game, velocity, type) {
+	this.game = game;
+	this.ennemySprite = null;
+	this.isDead = null;
+	this.velocity = velocity;
+	this.type = type;
+	this.cursors = null;
+	this.posX = 800;
+	this.posY = 566;
+	this.isDead = false;
 };
 
+var key1;
+var arr = ["fire", "water","plant"];
+var arrDead = ["firedead","waterdead","plantdead"];
 Ennemy.prototype.create = function create() {
-	
+    game.stage.backgroundColor = '#736357';
+
+
+    key1 = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+
+	// PHYSICS PROPERTIES
+
+	this.ennemySprite = this.game.add.sprite(this.posX,this.posY, arr[this.type]);
+
+	// PHYSICS PROPERTIES
+	this.ennemySprite.anchor.setTo(0.5, 0);
+	this.game.physics.arcade.enable(this.ennemySprite);
+	this.ennemySprite.enableBody = true;
+
+	this.ennemySprite.body.gravity.x = this.velocity;
+
+
 };
+
 
 Ennemy.prototype.update = function update() {
-	
 };
+
+
+Ennemy.prototype.setisDead = function setisDead(isdead) {
+
+	this.isDead = isDead;
+};
+
+Ennemy.prototype.getPosX = function getPosX(){
+	return this.ennemySprite.x;
+};
+
+Ennemy.prototype.getType = function getType() {
+
+	return arr[this.type];
+};
+
+Ennemy.prototype.destroy = function destroy () {
+
+    sprite.destroy();
+};
+
+Ennemy.prototype.kill = function kill () {
+
+	//DOSOMETHING
+};
+
 
 
 
