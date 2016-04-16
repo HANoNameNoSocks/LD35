5f18da21d9e6704c50445d11d41f078784187008
mabitesonhistoire <arthur.aubrun@gmail.com>
2016-04-16 19:40:02 +0200

    fix this fucking js

--- js/RefereeManager.js
+++ js/RefereeManager.js
@@ -11,16 +11,20 @@ function RefereeManager(game) {
 }
 
 RefereeManager.prototype.create = function create() {
+	var me = this;
+	console.log("start of prototype.create");
+	console.log("call of new ItemManager");
 	this.itemManager = new ItemManager(this.game);
 	this.tempNames = this.names;
-	this.itemManager.createItem(this.getData());
+	console.log("call of createItem");
+	this.itemManager.createItem(getData(me));
 	this.currentCounter = 0;
 }
 
 RefereeManager.prototype.update = function update() {
 };
 
-function judge(data) {
+RefereeManager.prototype.judge = function judge(data) {
 	if (data == 1) {
 		this.playerWonAFight()
 	} else if (data == 0) {
@@ -36,7 +40,7 @@ function playerWonAFight() {
 		if (++this.limit >= this.items.length) {
 			this.hasWon = true;
 		} else {
-			this.itemManager.createItem(this.getData());
+			this.itemManager.createItem(getData(me));
 			this.currentCounter = 0;
 		}
 	}
@@ -50,16 +54,16 @@ function playerLoseAFight() {
 	this.hasLost = true;
 };
 
-function getData() {
-	return {name : this.getName(), counter : Math.floor((Math.random() * 10) + 1), type : this.getType()};
+function getData(me) {
+	return {name : getName(me), counter : Math.floor((Math.random() * 10) + 1), type : getType()};
 };
 
-function getName() {
-	var name = this.tempNames[Math.floor((Math.random() * this.tempNames.length))];
-	var idx = this.tempNames.indexOf(name);
-	this.tempNames.splice(idx, 1);
-	if (this.tempNames.length == 0) {
-		this.tempNames = this.names;
+function getName(me) {
+	var name = me.tempNames[Math.floor((Math.random() * me.tempNames.length))];
+	var idx = me.tempNames.indexOf(name);
+	me.tempNames.splice(idx, 1);
+	if (me.tempNames.length == 0) {
+		me.tempNames = me.names;
 	}
 	return name;
 };
