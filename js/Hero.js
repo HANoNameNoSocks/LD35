caa3945196fc9534d067daca2fbd32526f3c19d0
unknown <dgfly.ha@gmail.com>
2016-04-16 21:10:00 +0200

    re? create asset img folder

--- js/Hero.js
+++ js/Hero.js
@@ -1,27 +1,57 @@
 function Hero(game) {
-	this.game = game ;
-	this.sprite = null ;
-	this.posX = 0 ;
-	this.posY = 0 ;
+	this.game = game;
+	this.sprite = null;
+	this.posX = 800;
+	this.posY = 566;
 
 	this.isDead = false;
+	this.isFighting = false;
+
+	this.cursors = null;
+	this.firebutton = null;
+	this.plant = null;
+	this.water = null;
 	
 };
 
 Hero.prototype.create = function create() {
-	this.sprite = this.game.add.sprite(150,480, 'hero');
-
+	this.sprite = this.game.add.sprite(this.posX,this.posY, 'hero');
 	this.game.physics.arcade.enable(this.sprite);
-/*	this.sprite.enableBody = true;
-	this.sprite.body.collideWorldBounds = true;*/
+
+	this.cursors = this.game.input.keyboard.createCursorKeys();
+
+	this.firebutton = this.game.input.keyboard.addKey(Phaser.Keyboard.W)
+	this.plantbutton = this.game.input.keyboard.addKey(Phaser.Keyboard.C)
+	this.waterbutton = this.game.input.keyboard.addKey(Phaser.Keyboard.X)
 };
 
 Hero.prototype.update = function update() {
-	
+	this.isFighting = false;
+	this.fight();
+};
+
+Hero.prototype.fight = function fight() {
+	if (this.firebutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "fire";
+		console.log("attack fire");
+	} else if (this.plantbutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "plant";
+		console.log("attack plant");
+	} else if (this.waterbutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "water";
+		console.log("attack water");
+	}
 };
 
 Hero.prototype.isDead = function isDead() {
 	return this.isDead;
+};
+
+Hero.prototype.getType = function getType() {
+	return this.attackType;
 };
 
 Hero.prototype.setIsDead = function isDead(isDead) {
@@ -29,7 +59,7 @@ Hero.prototype.setIsDead = function isDead(isDead) {
 };
 
 Hero.prototype.destroy = function destroy() {
-
+	this.sprite.destroy();
 };
 
 
--- js/preload.js
+++ js/preload.js
@@ -5,11 +5,11 @@ preload.prototype = {
         var loadingBar = this.add.sprite(160,240,"loading");
         loadingBar.anchor.setTo(0.5,0.5);
         this.load.setPreloadSprite(loadingBar);
-		this.game.load.image("gametitle","assets/gametitle.png");
-		this.game.load.image("play","assets/play.png");
-		this.game.load.image("gameover","assets/gameover.png");
-		this.game.load.image("victory","assets/victory.png");
-		this.game.load.image("playRules","assets/playRules.png");
+		this.game.load.image("gametitle","assets/img/gametitle.png");
+		this.game.load.image("play","assets/img/play.png");
+		this.game.load.image("gameover","assets/img/gameover.png");
+		this.game.load.image("victory","assets/img/victory.png");
+		this.game.load.image("playRules","assets/img/playRules.png");
 		
 		this.game.load.image('fire','assets/img/fire.png');
 		this.game.load.image('plant','assets/img/plant.png');
