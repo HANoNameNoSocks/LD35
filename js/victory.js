320ee710066a00de6f03ce5578a7ddd3db851b04
unknown <dgfly.ha@gmail.com>
2016-04-18 00:56:50 +0200

    change victory screen state + press enter

--- js/victory.js
+++ js/victory.js
@@ -12,10 +12,10 @@ victory.prototype = {
     		winSound.resume();
     	}	
 
-  		var gameOverTitle = this.game.add.sprite(160,160,"victory");
-		gameOverTitle.anchor.setTo(0.5,0.5);
-		var playButton = this.game.add.button(160,320,"play",this.playTheGame,this);
-		playButton.anchor.setTo(0.5,0.5);
+  		var victoryTitle = this.game.add.sprite(0,0,"victory");
+      
+      this.restartButton = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+      this.restartButton.onDown.add(this.playTheGame, this);
 	},
 	playTheGame: function(){
 		winSound.pause();
