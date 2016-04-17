4f81035ba88e0df3d302d1146317ad0f3403bf8c
boudesit <boudesit@gmail.com>
2016-04-17 13:24:30 +0200

    CLEAN HERO

--- js/Hero.js
+++ js/Hero.js
@@ -1,63 +1,73 @@
 function Hero(game) {
 	this.game = game;
 	this.sprite = null;
-	this.posX = 210;
-	this.posY = 566;
+	this.posX = 130;
+	this.posY = 450;
 
 	this.isDead = false;
 	this.isFighting = false;
 
 	this.cursors = null;
 	this.firebutton = null;
-	this.plantbutton = null;
-	this.waterbutton = null;
+	this.plant = null;
+	this.water = null;
 
 	this.attackType = null;
 	
 };
 
-<<<<<<< HEAD
 var shakeWorld = 0;
-// THIS IS A COMMENTARY
-=======
->>>>>>> a1ae664301e05bcc3fe62a1747f8456926df1d68
+
 Hero.prototype.create = function create() {
 	this.sprite = this.game.add.sprite(this.posX,this.posY, 'hero');
+	this.sprite.animations.add('idle', [1,2]);
 	this.game.physics.arcade.enable(this.sprite);
+	
+  	this.sprite.animations.add('idle', [1,2]);
+	this.sprite.animations.play('idle', 3000, true);
+	
+	this.cursors = this.game.input.keyboard.createCursorKeys();
 
 	this.firebutton = this.game.input.keyboard.addKey(Phaser.Keyboard.W)
 	this.plantbutton = this.game.input.keyboard.addKey(Phaser.Keyboard.C)
 	this.waterbutton = this.game.input.keyboard.addKey(Phaser.Keyboard.X)
-
-	var self = this;
-
-	this.firebutton.onDown.add(this.fire, this);
-	this.plantbutton.onDown.add(this.plant, this);
-	this.waterbutton.onDown.add(this.water, this);
 };
 
 Hero.prototype.update = function update() {
 	this.attackType = null;
 	this.isFighting = false;
-	/*this.fight();*/
+	this.fight();
+
+    if (shakeWorld > 0) 
+    {
+      var rand1 = game.rnd.integerInRange(-10,10);
+      var rand2 = game.rnd.integerInRange(-10,10);
+      game.world.setBounds(rand1, rand2, game.width + rand1, game.height + rand2);
+      shakeWorld--;
+    }
+
+    if (shakeWorld == 0) {
+      game.world.setBounds(0, 0, game.width,game.height);
+    }
 };
 
-Hero.prototype.fire = function fire() {
-	this.isFighting = true;
-	this.attackType = "fire";
-	console.log("attack fire");
-};
-
-Hero.prototype.plant = function plant() {
-	this.isFighting = true;
-	this.attackType = "plant";
-	console.log("attack plant");
-};
-
-Hero.prototype.water = function water() {
-	this.isFighting = true;
-	this.attackType = "water";
-	console.log("attack water");
+Hero.prototype.fight = function fight() {
+	if (this.firebutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "fire";
+		shakeWorld = 5;
+		console.log("attack fire");
+	} else if (this.plantbutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "plant";
+		shakeWorld = 5;
+		console.log("attack plant");
+	} else if (this.waterbutton.isDown) {
+		this.isFighting = true;
+		this.attackType = "water";
+		shakeWorld = 5;
+		console.log("attack water");
+	}
 };
 
 Hero.prototype.isDead = function isDead() {
