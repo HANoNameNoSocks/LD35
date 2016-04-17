cb05f1b13e9569e46494f1f05d6d9c5ead9bed58
Quentin <quentindegraeve59@gmail.com>
2016-04-17 12:28:17 +0200

    no commit

--- js/preload.js
+++ js/preload.js
@@ -18,11 +18,15 @@ preload.prototype = {
 		this.game.load.image('plantdead','assets/img/plantdead.png');
 		this.game.load.image('waterdead','assets/img/waterdead.png');
  	 	this.game.load.spritesheet('sprFire', 'assets/img/fireSprite.png', 39,40,16);
- 	 	this.game.load.spritesheet('sprPlant', 'assets/img/plantSprite.png', 39,40,16);
+ 	 	this.game.load.spritesheet('ennemyPlant', 'assets/img/ennemyPlant.png', 110,115,9);
  	 	this.game.load.spritesheet('sprWater', 'assets/img/waterSprite.png', 39,40,16);
 
+ 	 	this.game.load.spritesheet('sprAttaqueFire', 'assets/img/heroAttaqueFire.png', 39,40,16);
+ 	 	//this.game.load.spritesheet('sprAttaqurPlant', 'assets/img/heroAttaquePlant.png', 110,115,10);
+ 	 	this.game.load.spritesheet('sprAttaqueWater', 'assets/img/heroAttaqueWater.png', 39,40,16);
 
-		this.game.load.image("hero", "assets/img/hero.png");
+
+		this.game.load.image("hero", "assets/img/hero.png", 39,40,16);
 
 		this.game.load.image('fillAsset','assets/img/fillAsset.png');
 		this.game.load.image('emptyAsset','assets/img/emptyAsset.png');
