40eabd30862539c901b76e1ca9ed52bfae561ad5
mabitesonhistoire <arthur.aubrun@gmail.com>
2016-04-17 17:21:00 +0200

    fix

--- js/AssetCounter.js
+++ js/AssetCounter.js
@@ -1,13 +1,20 @@
 function AssetCounter(game) {
 	this.game = game;
 	this.sprite = null;
+	this.initialY;
 };
 
 AssetCounter.prototype.create = function create(x,y, assetType) {
+	this.initialY = y;
 	this.sprite = this.game.add.sprite(x, y, assetType);
 };
 
 AssetCounter.prototype.update = function update() {
+	if (this.sprite.y <= this.initialY - 20) {
+		this.sprite.body.gravity.y = 0;
+		this.sprite.body.velocity.y = 0;
+		this.sprite.y = this.initialY;
+	}
 };
 
 AssetCounter.prototype.getSprite = function getSprite() {
